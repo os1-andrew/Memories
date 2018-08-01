58fac55427c08426d4a18cf36462225dc1110a98
Andrew Liao <andrewoliao@gmail.com>
2018-08-01 14:00:28 -0700

    Build storyboards and begin implementing accompanying view controllers.

---
+++ Memories/Memories/OnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingViewController.swift
+//  Memories
+//
+//  Created by Andrew Dhan on 8/1/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+
+    @IBAction func finishOnboarding(_ sender: Any) {
+    }
+}
